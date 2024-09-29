1594fa473bdaa02d220a875b6bdc1d9a65f02bb9
DrMido <51366658+DrMido@users.noreply.github.com>
2024-09-29 08:17:13 +0300

    prettier

--- main.js
+++ main.js
@@ -1,96 +1,101 @@
-import './style.css'
-import * as THREE from 'three'
+import './style.css';
+import * as THREE from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
-})
+});
 
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(window.innerWidth, window.innerHeight)
-camera.position.setZ(30)
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
 
-renderer.render(scene, camera)
+renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 })
-const torus = new THREE.Mesh(geometry,material)
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const torus = new THREE.Mesh(geometry, material);
 
-scene.add(torus)
+scene.add(torus);
 
-const light = new THREE.DirectionalLight(0xffffff, 1)
-light.position.set(0, 10, 10)
-const lightAmbient = new THREE.AmbientLight(0xffffff, 1)
-scene.add(light, lightAmbient)
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 10, 10);
+const lightAmbient = new THREE.AmbientLight(0xffffff, 1);
+scene.add(light, lightAmbient);
 
-
-const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24)
-  const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
   const star = new THREE.Mesh(geometry, material);
 
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
 
-  star.position.set(x, y, z)
-  scene.add(star)
+  star.position.set(x, y, z);
+  scene.add(star);
 }
 
-Array(200).fill().forEach(addStar)
+Array(200).fill().forEach(addStar);
 
-const spaceTexture = new THREE.TextureLoader().load('space.jpg')
-scene.background = spaceTexture
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
 
-const midoTexture = new THREE.TextureLoader().load('Feltcute.jpg')
+const midoTexture = new THREE.TextureLoader().load('Feltcute.jpg');
 const mido = new THREE.Mesh(
   new THREE.BoxGeometry(3, 3, 3),
-  new THREE.MeshBasicMaterial({ map: midoTexture })
-)
+  new THREE.MeshBasicMaterial({ map: midoTexture }),
+);
 
-scene.add(mido)
+scene.add(mido);
 
-const earthTexture = new THREE.TextureLoader().load('earth.jpg')
+const earthTexture = new THREE.TextureLoader().load('earth.jpg');
 const earth = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshBasicMaterial({ map: earthTexture })
-)
+  new THREE.MeshBasicMaterial({ map: earthTexture }),
+);
 
-earth.position.z = 30
-earth.position.setX(-10)
+earth.position.z = 30;
+earth.position.setX(-10);
 
-scene.add(earth)
-
+scene.add(earth);
 
 function moveCamera() {
-  const t = document.body.getBoundingClientRect().top
-  earth.rotation.x += 0.05
-  earth.rotation.y += 0.075
-  earth.rotation.z += 0.05
-  mido.rotation.y += 0.1
-  mido.rotation.z += 0.1
+  const t = document.body.getBoundingClientRect().top;
+  earth.rotation.x += 0.05;
+  earth.rotation.y += 0.075;
+  earth.rotation.z += 0.05;
+  mido.rotation.y += 0.1;
+  mido.rotation.z += 0.1;
   camera.position.z = t * -0.01;
   camera.position.y = t * -0.0002;
   camera.position.x = t * -0.0002;
 }
 
-document.body.onscroll = moveCamera
+document.body.onscroll = moveCamera;
 
 function animate() {
-  requestAnimationFrame(animate)
+  requestAnimationFrame(animate);
 
-  torus.rotation.x += 0.01
-  torus.rotation.y += 0.005
-  torus.rotation.z += 0.01
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
 
-  controls.update()
+  controls.update();
 
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
 }
 
-animate()
+animate();
